7509f5134cd653fd9677081f58ad997d3a70a127
usmankhan-333 <usmankhan753099@gmail.com>
2026-08-19 17:46:13 +0500

    feat: add forgot password route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
@@ -11,6 +12,11 @@ function App() {
         />
 
         <Route path="/login" element={<Login />} />
+
+        <Route
+          path="/forgot-password"
+          element={<ForgotPassword />}
+        />
       </Routes>
     </BrowserRouter>
   );
